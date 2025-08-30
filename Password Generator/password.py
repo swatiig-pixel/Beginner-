af7ce8fb8c9b869d595f80bc571cbf70af810b67
Swati <swatiig1903@gmail.com>
2025-08-30 21:25:23 +0530

    Takes user input whether satisfied or not

--- Password Generator/password.py
+++ Password Generator/password.py
@@ -2,30 +2,35 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-print("Welcome to the PyPassword Generator!")
-nr_letters = int(input("How many letters would you like in your password?\n"))
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
+print("Welcome to the Password Generator!")
 
-import random
-#Easy level
-#
+satisfied = "no"
 
-#Hard level
-password_list = []
+while satisfied == "no":
+  nr_letters = int(input("How many letters would you like in your password?\n"))
+  nr_symbols = int(input(f"How many symbols would you like?\n"))
+  nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-for char in range(1,nr_letters +1):
-    password_list.append(random.choice(letters))
+  import random
+  password_list = []
 
-for sym in range(1,nr_symbols +1):
-    password_list.append(random.choice(symbols))
+  for char in range(1,nr_letters +1):
+      password_list.append(random.choice(letters))
 
-for num in range(1,nr_numbers +1):
-    password_list.append(random.choice(numbers))
+  for sym in range(1,nr_symbols +1):
+      password_list.append(random.choice(symbols))
+
+  for num in range(1,nr_numbers +1):
+      password_list.append(random.choice(numbers))
 
 
-random.shuffle(password_list)
+  random.shuffle(password_list)
 
-password = "".join(password_list)
+  password = "".join(password_list)
 
-print(f"Your Generated password is '{password}'")
+  print(f"Your Generated password is '{password}'")
+  satisfied = input("Are you satisfied with this password(Yes/No): ").lower()
+  
+
+print("Thank you ,Have a secure day")
+
