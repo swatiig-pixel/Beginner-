02d3321c99f1967c76406e8ab98f137bc76295c6
Swati <swatiig1903@gmail.com>
2025-09-01 21:01:43 +0530

    Added title

--- Simple calculator/using_turtle.py
+++ Simple calculator/using_turtle.py
@@ -1,7 +1,18 @@
 from turtle import Turtle, Screen
+from tkinter import *
 
 screen = Screen()
 screen.setup(width=400,height=600)
-screen.bgcolor("black")
+screen.bgcolor("white")
 screen.title("Calculator")
+t = Turtle()
+t.hideturtle()
+t.penup()
+t.goto(-140,250)
+t.pendown()
+t.color("blue")
+t.write("Python Calculator",font=("Arial",24,"bold"))
 screen.exitonclick()
+
+
+
