984c2da7a7cb0eb827ca3d5103b9f1bf7e32e209
Swati <swatiig1903@gmail.com>
2025-09-06 22:51:35 +0530

    Adding all the logic to with GUI

--- Converter/unit_converter.py
+++ Converter/unit_converter.py
@@ -2,67 +2,70 @@ from turtle import Turtle, Screen
 import turtle
 
 screen = Screen()
-screen.title("Temperture Converter")
-screen.bgcolor("lightblue")
+screen.title("Unit Converter")
+screen.bgcolor("lightgreen")
 
+result = 0
 
 
 def length_converter(choice):
+  global result
 
 
   if choice == 1:
-    cm = float(input("Enter length in cm: "))
-    print(f"{cm} cm = {cm/100:2f}m")
+    cm = float(screen.textinput("lenght","Enter length in cm: "))
+    result = f"{cm} cm = {cm/100:2f}m"
 
   elif choice == 2:
-    m = float(input("Enter lenght in meters: "))
-    print(f"{m} m = {m/1000:.2f} km")
+    m = float(screen.textinput("Length","Enter lenght in meters: "))
+    result = f"{m} m = {m/1000:.2f} km"
 
   elif choice == 3:
-    km = float(input("Enter length in kilometer: "))
-    print(f"{km} km = {km*0.621371:.2f} miles")
+    km = float(screen.textinput("Length","Enter length in kilometer: "))
+    result = f"{km} km = {km*0.621371:.2f} miles"
 
   else:
-    print("INVALID CHOICE!!!!")
+    result = "INVALID CHOICE!!!!"
 
 
-def weight_converter():
-  print("\nWEIGHT CONVERTER")
-  print("1.Grams to Kilograms")
-  print("2.Kilograms to pounds")
-  choice = int(input("Choose option (1/2): "))
+def weight_converter(choice):
+  global result
 
   if choice == 1:
-    g = float(input("Enter weight in grams: "))
-    print(f"{g} g = {g/1000:.2f} kg")
+    g = float(screen.textinput("Weight","Enter weight in grams: "))
+    result = f"{g} g = {g/1000:.2f} kg"
 
   elif choice == 2:
-    kg = float(input("Enter weight in Kilograms: "))
-    print(f"{kg} kg = {kg*2.20462:.2f} lbs")
+    kg = float(screen.textinput("Weight","Enter weight in Kilograms: "))
+    result = f"{kg} kg = {kg*2.20462:.2f} lbs"
 
   else:
-    print("INVALID CHOICE!!!")
+    result = "INVALID CHOICE!!!"
 
 
 user_choice = screen.textinput("Choose Conversion","Type 'A' Length Converter\nType 'B' Weight Converter")
 
 if user_choice and user_choice.upper() == "A":
-  t = turtle.Turtle()
-  t.hideturtle()
-  t.penup()
-  t.goto(-100,20)
-  t.pendown()
-  t.color("darkblue")
-  t.write("\nLENGTH CONVERTER\n1.Centimeters to Meters\n2.Meters to Kilometers\n3.Kilometers to Miles")
-  lenght_choice = screen.textinput("Enter your Choice(1/2/3):")
-  length_converter(lenght_choice)
-  screen.exitonclick()
+  length_choice = screen.textinput("\nLENGTH CONVERTER","\n1.Centimeters to Meters\n2.Meters to Kilometers\n3.Kilometers to Miles")
+  length_choice = int(length_choice)
+  length_converter(length_choice)
 
 
 elif user_choice and user_choice.upper() == "B":
-
-  weight_converter()
+  weight_choice = screen.textinput("\nWEIGHT CONVERTER\n","1.Grams to Kilograms\n2.Kilograms to pounds")
+  weight_choice = int(weight_choice)
+  weight_converter(weight_choice)
 
 else:
-  print("INVALID CHOICE!!!!")
+  result = "INVALID CHOICE!!!!"
   
+
+t = turtle.Turtle()
+t.hideturtle()
+t.penup()
+t.goto(-150,20)
+t.pendown()
+t.color("green")
+t.write(result, font=("Arial",20,"normal"))
+
+turtle.done()
