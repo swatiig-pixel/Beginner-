326d3f743802e52eb678990fd209e9276f54dc3f
Swati <swatiig1903@gmail.com>
2025-09-07 23:53:43 +0530

    New turtle game started

---
+++ Turtle game/turtle__.py
@@ -0,0 +1,25 @@
+from turtle import Turtle, Screen
+import turtle
+
+screen = Screen()
+screen.setup(width=400,height=400)
+screen.bgcolor("pink")
+
+t = turtle()
+t.shape("turtle")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
