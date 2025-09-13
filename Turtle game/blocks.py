23df85b8df083340524f6c30d227be60a631ad95
Swati <swatiig1903@gmail.com>
2025-09-13 11:17:32 +0530

    Game complete logic added

--- Turtle game/blocks.py
+++ Turtle game/blocks.py
@@ -1,13 +1,16 @@
 from turtle import Turtle
 import random
+import time
 
 COLORS = ["red","blue","yellow","green","orange","white","purple"]
+BLOCK_MOVING_DISTANCE = 5
+MOVE_INCREASE = 10
 
 class Blocks(Turtle):
   
   def __init__(self):
     self.all_blocks = []
-    self.move_speed = 1
+    self.move_speed = 0.1
 
   def move_speeder(self):
     self.move_speed *= 0.99
@@ -17,13 +20,21 @@ class Blocks(Turtle):
     if random_chance == 1:
       block = Turtle()
       block.shape("square")
-      block.shapesize(stretch_wid=2,stretch_len=1)
       block.penup()
+      block.shapesize(stretch_wid=1,stretch_len=2)
       block.color(random.choice(COLORS))
       new_y = random.randint(-250,250)
       block.goto(280,new_y)
-      self.all_blocks.append()
+      self.all_blocks.append(block)
 
+  def move_blocks(self):
+    for block in self.all_blocks:
+      block.backward(BLOCK_MOVING_DISTANCE)
+
+  def many_boxes(self):
+    for num in range(0,100):
+      self.create_blocks()
+      time.sleep(0.1)
 
 
 
--- Turtle game/main.py
+++ Turtle game/main.py
@@ -1,18 +1,35 @@
 from turtle import Turtle , Screen
 from turtle__ import Player
+from blocks import Blocks
+import time
 
 screen = Screen()
 screen.setup(600,600)
 screen.bgcolor("pink")
+screen.tracer(0)
 
 p = Player()
+b = Blocks()
+
 
 screen.listen()
 screen.onkey(p.space,"space")
 
-if p.ycor() > 280:
-  p.refresh()
+game_on = True
+while game_on:
+  time.sleep(b.move_speed)
+  screen.update()
+  b.create_blocks()
+  b.move_blocks()
 
+  for block in b.all_blocks:
+    if block.distance(p) < 20:
+      game_on = False
+
+  if p.ycor()> 280:
+    p.refresh()
+    for block in b.all_blocks:
+      b.move_speeder()
 
 
 
