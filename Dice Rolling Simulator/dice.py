fb8ad97e988921d133200f1bcd98208b8e0455cb
Swati <swatiig1903@gmail.com>
2025-09-16 13:13:13 +0530

    Dice

--- Dice Rolling Simulator/dice.py
+++ Dice Rolling Simulator/dice.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+import random
 
 class Dice(Turtle):
 
@@ -13,6 +14,18 @@ class Dots(Turtle):
 
   def __init__(self):
     super().__init__()
-    self.shape("circle")
-    self.color("black")
-    self.shapesize(stretch_len=0.5,stretch_wid=0.5)
+    numbers = random.randint(1,7)
+    for i in range(numbers):
+      self.shape("circle")
+      self.color("black")
+      self.shapesize(stretch_len=0.5,stretch_wid=0.5)
+      if numbers == 2:
+        for num in range(numbers-1):
+          two_num = [-5,5]
+          self.goto(two_num[num-1],0)
+          
+
+        
+
+
+    
--- Dice Rolling Simulator/main.py
+++ Dice Rolling Simulator/main.py
@@ -10,5 +10,7 @@ dot = Dots()
 
 
 
+
+
 screen.exitonclick()
 
