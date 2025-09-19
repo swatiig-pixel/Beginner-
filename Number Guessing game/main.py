e4b1113956f8c282115ba823962f27a893663f1f
Swati <swatiig1903@gmail.com>
2025-09-19 14:48:04 +0530

    Basic functions added to Number guessing

--- Number Guessing game/main.py
+++ Number Guessing game/main.py
@@ -8,6 +8,21 @@ t = Turtle()
 t.hideturtle()
 t.write("Guess the number between 1 - 100\nChoose your level\n1.Easy\n2.Hard",font=("Arial",19,"normal"),align="center")
 
+level = screen.textinput("Level Choosing","Easy or Hard")
+
+if level and level.upper() == "EASY":
+  screen.clear()
+  screen.bgcolor("pink")
+  t.goto(0,15)
+  t.write("You will be given 10 chances to guess the number",align="center",font=("Arial",19,"normal"))
+  t.goto(0,-15)
+  t.write("Click space to start",align="center",font=("Arial",19,"normal"))
+
+  def to_start():
+    screen.textinput("Enter Number","Between 1-100")
+
+  screen.listen()
+  screen.onkey(key="space",fun=to_start)
 
 
 screen.exitonclick()
