3e5f2e755cff0090bfc4d64741f06431d4685eef
Swati <swatiig1903@gmail.com>
2025-09-21 20:02:09 +0530

    Spirograph added

---
+++ Animations/spirograph.py
@@ -0,0 +1,27 @@
+from turtle import Turtle , Screen
+import turtle as tim
+import random
+
+t = Turtle()
+screen = Screen()
+screen.setup(600,600)
+screen.bgcolor("pink")
+tim.colormode(255)
+t.speed("fastest")
+
+def random_color():
+  r = random.randint(0,255)
+  g = random.randint(0,255)
+  b = random.randint(0,255)
+  random_col = (r,g,b)
+  return random_col
+
+for num in range(100):
+  t.circle(50)
+  t.right(random.randint(1,25))
+  t.color(random_color())
+
+
+
+
+screen.exitonclick()
