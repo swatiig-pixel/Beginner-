6fab204aa8b5a4596e7c471ec03b52e3893ea711
Swati <swatiig1903@gmail.com>
2025-09-24 23:53:07 +0530

    Functions added

--- Animations/paint.py
+++ Animations/paint.py
@@ -4,7 +4,28 @@ setup(600,600)
 
 hideturtle()
 
+def move():
+  forward(10)
 
+def left():
+  left(15)
+
+def right():
+  right(15)
+
+def upwards():
+  setheading(90)
+
+def downwards():
+  setheading(270)
+
+def right_curve():
+  forward(5)
+  right(5)
+
+def left_curve():
+  forward(5)
+  left(5)
 
 
 
